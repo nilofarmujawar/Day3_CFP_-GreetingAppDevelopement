f47534dc6360fd4adef1b2a2d98b45e5186a8d96
Nilofar Mujawar <99306976+nilofarmujawar@users.noreply.github.com>
2022-04-21 05:38:19 -0700

    Added: Extend greeting controller to use service layer to get simple greeting UC2.

--- src/main/java/com/bridgelabz/greetingapplication/service/GreetingService.java
+++ src/main/java/com/bridgelabz/greetingapplication/service/GreetingService.java
@@ -1,21 +1,10 @@
 package com.bridgelabz.greetingapplication.service;
 
-/**
- * import
- */
 import org.springframework.stereotype.Service;
 
-/**
- * 1) @Service :-
- *          It is used to mark the class as a service provider.
- *          So overall @Service annotation is used with classes that provide some business functionalities.
- *          Spring context will autodetect these classes when annotation-based configuration and classpath scanning is used.
- */
 @Service
-//create a class name as greetingService
 public class GreetingService {
 
-    //create a method name as getMessge
     public String getMessage() {
         return "Hello World";
     }
