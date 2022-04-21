dc9110d941468c7ceacbcd887d9b45cabf762de5
Nilofar Mujawar <99306976+nilofarmujawar@users.noreply.github.com>
2022-04-21 05:12:36 -0700

    Added:  Ability to give differnt greeting message with first name and last name UC3.

--- src/main/java/com/bridgelabz/greetingapplication/service/GreetingService.java
+++ src/main/java/com/bridgelabz/greetingapplication/service/GreetingService.java
@@ -1,30 +1,18 @@
 package com.bridgelabz.greetingapplication.service;
 
-/**
- * import
- */
 import org.springframework.stereotype.Service;
 
-/**
- * 1) @Service :-
- *          It is used to mark the class as a service provider.
- *          So overall @Service annotation is used with classes that provide some business functionalities.
- *          Spring context will autodetect these classes when annotation-based configuration and classpath scanning is used.
- */
 @Service
-//create a class name as greetingService
 public class GreetingService {
 
-    //create a method name as getMessge
     public String getMessage() {
         return "Hello World";
     }
-    //create a method name as getGreetingMessage
+
     public String getGreetingMessage(String firstName, String lastName) {
         return "Hello " + firstName + "" + lastName;
     }
 
-    //create a method name as postMessage
     public String postMessage(String firstName, String lastName) {
         return "Hello " + firstName + "" + lastName;
     }
