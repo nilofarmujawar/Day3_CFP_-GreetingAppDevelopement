6a929a6a3265b2c2ad97ed7595227cd993a25106
Nilofar Mujawar <99306976+nilofarmujawar@users.noreply.github.com>
2022-04-21 05:11:31 -0700

    Added: Ability to give differnt greeting message with first name and last name UC3

--- src/main/java/com/bridgelabz/greetingapplication/model/User.java
+++ src/main/java/com/bridgelabz/greetingapplication/model/User.java
@@ -1,37 +1,16 @@
 package com.bridgelabz.greetingapplication.model;
 
-/**
- * creating a class name as User
- */
 public class User {
-    /**
-     * variables
-     */
     private String firstName;
     private String lastName;
 
-    /**
-     * default constructor
-     */
     public User() {
 
     }
-
-    /**
-     * create a parameterized constructor
-     *
-     */
     public User(User user) {
         this.firstName=user.firstName;
         this.lastName=user.lastName;
     }
-
-    /**
-     * Used getter and setter to set and get the value.
-     * Setter is used to set the value
-     * Getter is used to get the value
-     */
-
     public String getfirstName() {
         return firstName;
     }
